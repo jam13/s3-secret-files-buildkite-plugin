8c4c5695804c54c54d421f3f9e5d7cd62ab147dc
Ben Spencer <ben.spencer@healthforge.io>
2019-01-28 09:34:49 +0000

    Add support for additional args to cp command.

--- lib/shared.bash
+++ lib/shared.bash
@@ -21,13 +21,14 @@ s3_download() {
   local bucket="$1"
   local key="$2"
   local dest="$3"
+  local extra_args="$4"
   local aws_s3_args=("--quiet" "--region=$AWS_DEFAULT_REGION")
 
   if [[ "${BUILDKITE_USE_KMS:-true}" =~ ^(true|1)$ ]] ; then
     aws_s3_args+=("--sse" "aws:kms")
   fi
 
-  if ! aws s3 cp "${aws_s3_args[@]}" "s3://$1/$2" "$3" ; then
+  if ! aws s3 cp "${aws_s3_args[@]}" $extra_args "s3://$1/$2" "$3" ; then
     exit 1
   fi
 }
